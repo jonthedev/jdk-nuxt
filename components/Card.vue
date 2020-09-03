56a8da2677208cef3659cbb6050dd90dda911a5b
darkthedev <darkthedev.x@gmail.com>
2020-09-03 10:51:34 +0200

    setup home page, added gsap animations

---
+++ components/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card">
+    <h2>
+      <slot name="title"></slot>
+    </h2>
+    <h3>
+      <slot name="job"></slot>
+    </h3>
+    <p>
+      <slot name="projects"></slot>
+    </p>
+    <slot name="svg"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.8);
+  font-size: 3rem;
+  color: var(--text-light);
+  height: 90%;
+  width: 90%;
+  border-radius: 7px;
+  opacity: 0;
+  transform: translateY(10rem);
+
+  border-bottom: 8px solid var(--text-light);
+  border-top: 2px solid var(--text-light);
+
+  padding: 0.5rem;
+
+  align-self: center;
+  justify-self: center;
+
+  text-align: center;
+}
+
+p {
+  font-size: 1.2rem;
+}
+
+h2 {
+  font-size: 2.8rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+@media screen and (max-width: 700px) {
+  h2 {
+    font-size: 1.8rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  h2 {
+    font-size: 1.7rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <main id="home" class="home-grid">
+      <card>
+        <template v-slot:title>Hi, I'm Jonathan.</template>
+        <template v-slot:job>Front End Developer.</template>
+      </card>
+      <card>
+        <template v-slot:svg>
+          <DeveloperSVG />
+        </template>
+      </card>
+      <card>
+        <template v-slot:projects
+          >I build websites and web applications.</template
+        >
+      </card>
+    </main>
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card";
+import DeveloperSVG from "../components/DeveloperSVG";
+import gsap from "gsap";
+
+export default {
+  mounted() {
+    gsap.to(".card", {
+      duration: 2,
+      y: 0,
+      ease: "bounce",
+      opacity: 1,
+      stagger: 0.25,
+    });
+  },
+  components: {
+    Card,
+    DeveloperSVG,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#home {
+  height: 80vh;
+  padding: 2rem;
+}
+.home-grid {
+  display: grid;
+
+  /* Explicit grid */
+  grid-template-areas:
+    "a b"
+    "c d";
+
+  grid-template-rows: repeat(2, 50%);
+  grid-template-columns: repeat(2, 50%);
+}
+
+/* INTRO */
+.card:nth-child(1) {
+  grid-area: a;
+}
+/* SVG DEVELOPER ANIMATION */
+.card:nth-child(2) {
+  grid-area: b;
+}
+/* WHAT I DO */
+.card:nth-child(3) {
+  grid-area: c;
+}
+
+/* Medium screens */
+@media screen and (max-width: 700px) {
+  .home-grid {
+    grid-template-areas:
+      "a"
+      "b"
+      "c"
+      "d";
+    grid-template-rows: repeat(3, 31%);
+    grid-template-columns: 1fr;
+  }
+}
+</style>
